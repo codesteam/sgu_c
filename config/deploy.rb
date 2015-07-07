d4c7de09b305e3239ec2cc574827b57af25ce632
stepozer <stepozer@gmail.com>
2015-07-07 22:53:38 +0300

    Use phpdotenv in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,31 +5,22 @@ lock '3.4.0'
 
 set :application, 'SGUC'
 set :repo_url, 'git@github.com:codesteam/sgu_c.git'
+set :scm, :git
+set :keep_releases, 5
 
-# Default branch is :master
+# Git branch
 set :branch, 'master'
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, ->{ fetch(:node_config)['app_dir'] }
+# Deploy directory
+set :deploy_to, ->{ fetch(:deploy_to) }
 
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for linked_dirs is []
+# Shared folders between all releases
 set :linked_dirs, %w{runtime vendor web/assets web/uploads}
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
 
 namespace :deploy do
 
   task :app_setup do
     on roles(:web) do
-      # Env config for app
-      env_php = "<?php \n return json_decode('#{fetch(:node_config)['app_env_options'].to_json}', 1);"
-      upload! StringIO.new(env_php), "#{current_path}/config/env.php"
-      execute "chmod 644 #{current_path}/config/env.php"
-
       # TODO: fix it use Yii2 assets
       execute "chmod -R 777 #{current_path}/web/assets_app"
 
@@ -42,6 +33,5 @@ namespace :deploy do
   end
 
   after :finishing, 'deploy:app_setup'
-  after :finishing, 'deploy:cleanup'
 
 end
--- config/deploy/production.example.rb
+++ config/deploy/production.example.rb
@@ -1,16 +1,13 @@
-# Setup node config
-node_config = {
-  'app_dir'         => '/path/to/sguc',
-  'app_role_web'    => 'user@host',
-  'app_env_options' => {
-    'yii_debug' => 1,
-    'yii_env'   => 'dev',
-    'db_name'   => '',
-    'db_user'   => '',
-    'db_pass'   => '',
-  },
-}
-set :node_config, node_config
+# Server credentials
+role :web, 'user@host'
 
-# Setup web role
-role :web, fetch(:node_config)['app_role_web']
+# Deploy directory
+set :deploy_to, '/path/to/sguc'
+
+# Env variables
+Capistrano::Env.use do |env|
+  env.add 'DB_NAME', ''
+  env.add 'DB_USER', ''
+  env.add 'DB_PASS', ''
+  env.filemode = 0644
+end
