73c424f2c97bbdb847065ba981e5e2f0231c8eca
stepozer <stepozer@gmail.com>
2015-07-11 15:40:37 +0300

    Fix

--- migrations/m150711_123451_rbac.php
+++ migrations/m150711_123451_rbac.php
@@ -1,44 +1,26 @@
 <?php
 
-use yii\base\InvalidConfigException;
 use yii\db\Schema;
 use yii\db\Migration;
 
 class m150711_123451_rbac extends Migration
 {
-    /**
-     * @throws yii\base\InvalidConfigException
-     * @return DbManager
-     */
-    protected function getAuthManager()
-    {
-        $authManager = Yii::$app->getAuthManager();
-        if (!$authManager instanceof DbManager) {
-            throw new InvalidConfigException('You should configure "authManager" component to use database before executing this migration.');
-        }
-        return $authManager;
-    }
-
     public function up()
     {
-        $authManager = $this->getAuthManager();
-        $this->db = $authManager->db;
+        $itemTable = '{{%auth_item}}';
+        $itemChildTable = '{{%auth_item_child}}';
+        $assignmentTable = '{{%auth_assignment}}';
+        $ruleTable = '{{%auth_rule}}';
 
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
-
-        $this->createTable($authManager->ruleTable, [
+        $this->createTable($ruleTable, [
             'name' => Schema::TYPE_STRING . '(64) NOT NULL',
             'data' => Schema::TYPE_TEXT,
             'created_at' => Schema::TYPE_INTEGER,
             'updated_at' => Schema::TYPE_INTEGER,
             'PRIMARY KEY (name)',
-        ], $tableOptions);
+        ]);
 
-        $this->createTable($authManager->itemTable, [
+        $this->createTable($itemTable, [
             'name' => Schema::TYPE_STRING . '(64) NOT NULL',
             'type' => Schema::TYPE_INTEGER . ' NOT NULL',
             'description' => Schema::TYPE_TEXT,
@@ -47,25 +29,25 @@ class m150711_123451_rbac extends Migration
             'created_at' => Schema::TYPE_INTEGER,
             'updated_at' => Schema::TYPE_INTEGER,
             'PRIMARY KEY (name)',
-            'FOREIGN KEY (rule_name) REFERENCES ' . $authManager->ruleTable . ' (name) ON DELETE SET NULL ON UPDATE CASCADE',
-        ], $tableOptions);
-        $this->createIndex('idx-auth_item-type', $authManager->itemTable, 'type');
+            'FOREIGN KEY (rule_name) REFERENCES ' . $ruleTable . ' (name) ON DELETE SET NULL ON UPDATE CASCADE',
+        ]);
+        $this->createIndex('idx-auth_item-type', $itemTable, 'type');
 
-        $this->createTable($authManager->itemChildTable, [
+        $this->createTable($itemChildTable, [
             'parent' => Schema::TYPE_STRING . '(64) NOT NULL',
             'child' => Schema::TYPE_STRING . '(64) NOT NULL',
             'PRIMARY KEY (parent, child)',
-            'FOREIGN KEY (parent) REFERENCES ' . $authManager->itemTable . ' (name) ON DELETE CASCADE ON UPDATE CASCADE',
-            'FOREIGN KEY (child) REFERENCES ' . $authManager->itemTable . ' (name) ON DELETE CASCADE ON UPDATE CASCADE',
-        ], $tableOptions);
+            'FOREIGN KEY (parent) REFERENCES ' . $itemTable . ' (name) ON DELETE CASCADE ON UPDATE CASCADE',
+            'FOREIGN KEY (child) REFERENCES ' . $itemTable . ' (name) ON DELETE CASCADE ON UPDATE CASCADE',
+        ]);
 
-        $this->createTable($authManager->assignmentTable, [
+        $this->createTable($assignmentTable, [
             'item_name' => Schema::TYPE_STRING . '(64) NOT NULL',
             'user_id' => Schema::TYPE_STRING . '(64) NOT NULL',
             'created_at' => Schema::TYPE_INTEGER,
             'PRIMARY KEY (item_name, user_id)',
-            'FOREIGN KEY (item_name) REFERENCES ' . $authManager->itemTable . ' (name) ON DELETE CASCADE ON UPDATE CASCADE',
-        ], $tableOptions);
+            'FOREIGN KEY (item_name) REFERENCES ' . $itemTable . ' (name) ON DELETE CASCADE ON UPDATE CASCADE',
+        ]);
     }
 
     public function down()
