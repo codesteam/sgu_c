b23d321fc217fd8c6a3690c0c2a03905e27b0e41
dstepanenko <studos.work@gmail.com>
2018-03-26 20:38:56 +0300

    fix contacts page

--- views/static/contact.php
+++ views/static/contact.php
@@ -8,9 +8,9 @@ $this->params['topMenu'] = 'contact';
 <div class="site-login">
     <?=HtmlApplication::h1(Html::encode($this->title))?>
     <p><strong>Контакты для оперативной связи: </strong></p>
-    <p>e-mail: KafIS_sevsu@sevsu.ru; тел.: +7(869) 241-77-41 (*1070)</p>
+    <p>e-mail: KafIS_sevsu@sevsu.ru; тел.: +7(8692) 41-77-41 (*1070)</p>
     <p><strong>Организационные вопросы: </strong></p>
-    <p>Шумейко Ирина Петровна, тел.: +7(978) 80-98-75</p>
+    <p>Шумейко Ирина Петровна, тел.: +7(978) 804-98-75</p>
     <p><strong>Ученые секретари: </strong></p>
     <p>Кротов Кирилл Викторович, тел.: +7(978) 730-38-19</p>
     <p>Безуглая Анна Евгеньевна, e-mail: anna_bezuglaya@list.ru</p>
