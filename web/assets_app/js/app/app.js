7e87bc542cb04c633be8f27aaf54cb35e2793668
dstepanenko <studos.work@gmail.com>
2018-04-17 20:55:39 +0300

    news

--- web/assets_app/js/app/app.js
+++ web/assets_app/js/app/app.js
@@ -2,3 +2,14 @@
   angular.module('SgucApp', []);
 
 }).call(this);
+
+$(document).ready(function(){
+    $('.news-read-mode').on('click', function() {
+        $('.news-read-mode').hide();
+        $('.news-read-mode-content').removeClass('hidden');
+    })
+    $('.news-read-hide').on('click', function() {
+        $('.news-read-mode').show();
+        $('.news-read-mode-content').addClass('hidden');
+    })
+})
