65853cb235f3ba91af174156687cb2d1084c2507
dstepanenko <studos.work@gmail.com>
2019-05-27 21:53:41 +0300

    change url

--- mail/application/created_for_admin.php
+++ mail/application/created_for_admin.php
@@ -5,7 +5,7 @@ require __DIR__.'/../layouts/_css.php';
 $this->title = 'Новая заявка на участие в конференции #'.$application->id;
 
 $urlRoot        = Url::to('/', 1);
-$urlApplication = Url::to(['/site/application-view', 'id' => $application->id, 'hash' => $application->hash], 1);
+$urlApplication = Url::to(['/admin/application', 'id' => $application->id], 1);
 ?>
 <h1 style="<?=$styleH1?>"><?= $this->title ?></h1>
 
@@ -14,6 +14,6 @@ $urlApplication = Url::to(['/site/application-view', 'id' => $application->id, '
     <a href="<?=$urlRoot?>" style="<?=$styleLink?>"><?=$urlRoot?></a>.
 </p>
 <p style="<?=$styleP?>">
-    Текущий статус вашей заявки можно узнать по ссылке
+    Статус вашей заявки можно узнать по ссылке
     <a href="<?=$urlApplication?>" style="<?=$styleLink?>"><?=$urlApplication?></a>.
 </p>
