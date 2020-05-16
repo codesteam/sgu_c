40c09aaa4292750ac360ed597ce0395258c67171
d.stepanenko <d.stepanenko@crpo.su>
2020-05-16 17:17:15 +0300

    add docker for local env

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ use app\helpers\Env;
 
 return [
     'class'    => 'yii\db\Connection',
-    'dsn'      => 'mysql:host=localhost;dbname='.$_ENV['DB_NAME'],
+    'dsn'      => 'mysql:host='.$_ENV['DB_HOST'].';port='.$_ENV['DB_PORT'].';dbname='.$_ENV['DB_NAME'],
     'username' => $_ENV['DB_USER'],
     'password' => $_ENV['DB_PASS'],
     'charset'  => 'utf8',
---
+++ ssh.sh
@@ -0,0 +1,1 @@
+docker-compose exec sguc /bin/bash
